cd4e034cc3c0d67039b6f4a1129c706f05954c21
ngammoudi <ngammoudi@exoplatform.com>
2014-06-27 17:05:26 +0100

    Task Management Juzu Portlet

---
+++ lib/src/main/java/org/exoplatform/task/Project.java
@@ -0,0 +1,58 @@
+package org.exoplatform.task;
+
+import java.util.Date;
+
+/**
+ * Created by nagui on 27/06/14.
+ */
+public class Project {
+    private String id;
+
+    private String name;
+
+    private String owner;
+
+    private String desc;
+
+    private Date dateCreated;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+}
---
+++ lib/src/main/java/org/exoplatform/task/Task.java
@@ -0,0 +1,100 @@
+package org.exoplatform.task;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by nagui on 27/06/14.
+ */
+public class Task  {
+
+    private String id;
+
+    private String projectId;
+
+    private String reporter;
+
+    private List<String> assignee;
+
+    private String status;
+
+    public String title;
+
+    private Date createdDate;
+
+    private Date startedDate;
+
+    private Date endDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(String projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getReporter() {
+        return reporter;
+    }
+
+    public void setReporter(String reporter) {
+        this.reporter = reporter;
+    }
+
+    public List<String> getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(List<String> assignee) {
+        this.assignee = assignee;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getStartedDate() {
+        return startedDate;
+    }
+
+    public void setStartedDate(Date startedDate) {
+        this.startedDate = startedDate;
+    }
+}
