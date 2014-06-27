2a9ee3119de7cfff0117fa7a8b957fef907def6f
ngammoudi <ngammoudi@exoplatform.com>
2014-06-27 15:14:18 +0100

    Task Management Juzu Portlet

---
+++ webapp/src/main/java/org/exoplatform/juzu/task/package-info.java
@@ -0,0 +1,12 @@
+@Application
+@Portlet
+@Bindings({
+
+}
+)
+
+package org.exoplatform.juzu.task;
+
+import juzu.Application;
+import juzu.plugin.portlet.Portlet;
+import juzu.plugin.binding.Bindings;
